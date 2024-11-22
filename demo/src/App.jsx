eda7f5fc96d8e7e72ba2e70709b7771526097656
Quayjn <briandkatz@gmail.com>
2024-11-21 20:47:12 -0800

    Refinements and demo improvements

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,46 +1,66 @@
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, GizmoHelper, GizmoViewport } from '@react-three/drei';
-import { useControls } from 'leva';
-import CartesianAxes from './components/R3FCartesianAxes';
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, GizmoHelper, GizmoViewport } from "@react-three/drei";
+import { useControls } from "leva";
+import CartesianAxes from "./components/R3FCartesianAxes";
+import "./App.css";
 
 function App() {
-
   const axesConfig = useControls({
-    xColor: '#9d4b4b', 
-    yColor: '#2f7f4f',
-    zColor: '#3b5b9d', 
+    xColor: "#9d4b4b",
+    yColor: "#2f7f4f",
+    zColor: "#3b5b9d",
     tickspacing: 50,
     ticklength: 15,
     thickness: 1,
     depth: 10000,
     includeLabels: true,
     includeX: true,
-    includeY: true, 
-    includeZ: false
-  })
+    includeY: true,
+    includeZ: false,
+  });
 
   return (
-    <>
-      <Canvas gl = {{antialias:true}}
-              style={{width:'calc(100vw - 30px)', height:'calc(100vh - 70px)',
-                      left:'10px', top:'50px', backgroundColor:'#EDEDCC'}}
-                      camera={{ position:[0, 0, 100
-                      ], fov:50, near:0.1, far:100000, makeDefault:true }}>                      
-        
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[2, 2, 2]} />
-        <OrbitControls />
+    <Canvas
+      className="demo"
+      gl={{ antialias: true }}
+      camera={{
+        position: [0, 0, 100],
+        fov: 50,
+        near: 0.1,
+        far: 100000,
+        makeDefault: true,
+      }}
+    >
+      <CartesianAxes {...axesConfig} />
 
-        <CartesianAxes {...axesConfig} />
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[2, 2, 2]} />
+      <OrbitControls />
+      <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
+        <GizmoViewport
+          axisColors={[axesConfig.xColor, axesConfig.yColor, axesConfig.zColor]}
+          labelColor="white"
+        />
+      </GizmoHelper>
 
-        <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
-          <GizmoViewport 
-            axisColors={[axesConfig.xColor, axesConfig.yColor, axesConfig.zColor]}
-            labelColor="white" />
-        </GizmoHelper>
+      <mesh position={[50, 50, -30]}>
+        <sphereGeometry args={[20, 32, 32]} />
+        <meshStandardMaterial
+          color={"#B29999"}
+          transparent={false}
+          depthWrite={true}
+        />
+      </mesh>
 
-      </Canvas>
-    </>
+      <mesh position={[-50, -50, 20]}>
+        <sphereGeometry args={[20, 32, 32]} />
+        <meshStandardMaterial
+          color={"#9999B2"}
+          transparent={false}
+          depthWrite={true}
+        />
+      </mesh>
+    </Canvas>
   );
 }
 
--- src/R3FCartesianAxes.jsx
+++ src/R3FCartesianAxes.jsx
@@ -4,142 +4,266 @@
  * Author: Quayjn aka Rudy Ruidoso
  * Version: 1.0.0
  * License: MIT (or any other applicable license)
- * Last Modified: 2024-10-29
+ * Last Modified: 2024-11-20
  */
-import {Text} from "@react-three/drei";
+import { Text } from "@react-three/drei";
+import { useEffect, useRef } from "react";
 
 export default function CartesianAxes({
-  includeX=true, includeY=true, includeZ=true,
-  xColor=0x9d4b4b, yColor=0x2f7f4f, zColor=0x3b5b9d, 
-  thickness=1, depth=2000, tickspacing=20, ticklength=15, includeLabels=true
-  }) { 
-
-  if (!(includeX || includeY || includeZ)) { return (<></>); }
+  includeX = true,
+  includeY = true,
+  includeZ = true,
+  xColor = 0x9d4b4b,
+  yColor = 0x2f7f4f,
+  zColor = 0x3b5b9d,
+  thickness = 1,
+  depth = 2000,
+  tickspacing = 20,
+  ticklength = 15,
+  includeLabels = true,
+}) {
+  if (!(includeX || includeY || includeZ)) {
+    return <></>;
+  }
 
   const axes = [];
   const ticksX = [];
   const ticksY = [];
   const ticksZ = [];
+  const meshRef = useRef();
 
-  var strLabelPos, strLabelNeg
+  var strLabelPos, strLabelNeg;
 
-  if (includeX) { 
+  if (includeX) {
     axes.push(
-      <mesh position={[0, 0, 0]} rotation={[0, 0, Math.PI/2]} key={`axisx`} >
+      <mesh position={[0, 0, 0]} rotation={[0, 0, Math.PI / 2]} key={`axisx`}>
         <cylinderGeometry args={[thickness, thickness, 2 * depth, 16]} />
-        <meshBasicMaterial color={xColor} /> 
+        <meshBasicMaterial
+          color={xColor}
+          depthWrite={true}
+          transparent={false}
+        />
       </mesh>
     );
     if (tickspacing > 0) {
-      for (var i=tickspacing; i<=depth; i+=tickspacing) {
+      for (var i = tickspacing; i <= depth; i += tickspacing) {
         strLabelPos = strLabelNeg = "";
         if (includeLabels) {
           strLabelPos = (
-            <Text  key={`labelx-${i}`}
-              position={[0, -(1 + ticklength/2), 0]} rotation={[0, 0, 0]} 
-              anchorX="center" anchorY="top" color={xColor} 
-              fontSize={thickness * tickspacing/3}>{i.toFixed(0)}</Text>
-          )
+            <Text
+              key={`labelx-${i}`}
+              position={[0, -(1 + ticklength / 2), 0]}
+              rotation={[0, 0, 0]}
+              anchorX="center"
+              anchorY="top"
+              color={xColor}
+              fontSize={(thickness * tickspacing) / 3}
+            >
+              {i.toFixed(0)}
+            </Text>
+          );
           strLabelNeg = (
-            <Text  key={`labelx-${i}-neg`}
-              position={[0, -(1 + ticklength/2), 0]} rotation={[0, 0, 0]} 
-              anchorX="center" anchorY="top" color={xColor}
-              fontSize={thickness * tickspacing/3}>-{i.toFixed(0)}</Text>)
+            <Text
+              key={`labelx-${i}-neg`}
+              position={[0, -(1 + ticklength / 2), 0]}
+              rotation={[0, 0, 0]}
+              anchorX="center"
+              anchorY="top"
+              color={xColor}
+              fontSize={(thickness * tickspacing) / 3}
+            >
+              -{i.toFixed(0)}
+            </Text>
+          );
         }
         ticksX.push(
-          <mesh position={[i, 0, 0]} rotation={[0, 0, 0]} key={`tickx-${i}-pos`}> 
-            <cylinderGeometry args={[thickness/2, thickness/2, ticklength, 16]} />
-            <meshBasicMaterial color={xColor} />{strLabelPos}</mesh>
+          <mesh
+            position={[i, 0, 0]}
+            rotation={[0, 0, 0]}
+            key={`tickx-${i}-pos`}
+          >
+            <cylinderGeometry
+              args={[thickness / 2, thickness / 2, ticklength, 16]}
+            />
+            <meshBasicMaterial
+              color={xColor}
+              depthWrite={true}
+              transparent={false}
+            />
+            {strLabelPos}
+          </mesh>
         );
         ticksX.push(
-          <mesh position={[-i, 0, 0]} rotation={[0, 0, 0]} key={`tickx-${i}-neg`}> 
-            <cylinderGeometry args={[thickness/2, thickness/2, ticklength, 16]} />
-            <meshBasicMaterial color={xColor} />{strLabelNeg}</mesh>
+          <mesh
+            position={[-i, 0, 0]}
+            rotation={[0, 0, 0]}
+            key={`tickx-${i}-neg`}
+          >
+            <cylinderGeometry
+              args={[thickness / 2, thickness / 2, ticklength, 16]}
+            />
+            <meshBasicMaterial
+              color={xColor}
+              depthWrite={true}
+              transparent={false}
+            />
+            {strLabelNeg}
+          </mesh>
         );
       }
     }
   }
 
-  if (includeY) { 
+  if (includeY) {
     axes.push(
       <mesh position={[0, 0, 0]} rotation={[0, 0, 0]} key={`axisy`}>
         <cylinderGeometry args={[thickness, thickness, 2 * depth, 16]} />
-        <meshBasicMaterial color={yColor} />
+        <meshBasicMaterial
+          color={yColor}
+          depthWrite={true}
+          transparent={false}
+        />
       </mesh>
     );
     if (tickspacing > 0) {
-      for (var i=tickspacing; i<=depth; i+=tickspacing) {
+      for (var i = tickspacing; i <= depth; i += tickspacing) {
         strLabelPos = strLabelNeg = "";
         if (includeLabels) {
           strLabelPos = (
-            <Text  key={`labely-${i}`}
-              position={[0, 1 + ticklength/2, 0]} rotation={[0, 0, -Math.PI/2]} 
-              anchorX="right" anchorY="middle" color={yColor} 
-              fontSize={thickness * tickspacing/3}>{i.toFixed(0)}</Text>
-          )
-          strLabelNeg = ( 
-            <Text  key={`labely-${i}-neg`}
-              position={[0, 1 + ticklength/2, 0]} rotation={[0, 0, -Math.PI/2]}
-              anchorX="right" anchorY="middle" color={yColor} 
-              fontSize={thickness * tickspacing/3}>-{i.toFixed(0)}</Text>
-          )
+            <Text
+              key={`labely-${i}`}
+              position={[0, 1 + ticklength / 2, 0]}
+              rotation={[0, 0, -Math.PI / 2]}
+              anchorX="right"
+              anchorY="middle"
+              color={yColor}
+              fontSize={(thickness * tickspacing) / 3}
+            >
+              {i.toFixed(0)}
+            </Text>
+          );
+          strLabelNeg = (
+            <Text
+              key={`labely-${i}-neg`}
+              position={[0, 1 + ticklength / 2, 0]}
+              rotation={[0, 0, -Math.PI / 2]}
+              anchorX="right"
+              anchorY="middle"
+              color={yColor}
+              fontSize={(thickness * tickspacing) / 3}
+            >
+              -{i.toFixed(0)}
+            </Text>
+          );
         }
 
         ticksY.push(
-          <mesh position={[0, i, 0]} rotation={[0, 0, Math.PI/2]} key={`ticky-${i}-pos`}> 
-            <cylinderGeometry args={[thickness/2, thickness/2, ticklength, 16]} />
-            <meshBasicMaterial color={yColor} />{strLabelPos}</mesh>
-        )
+          <mesh
+            position={[0, i, 0]}
+            rotation={[0, 0, Math.PI / 2]}
+            key={`ticky-${i}-pos`}
+          >
+            <cylinderGeometry
+              args={[thickness / 2, thickness / 2, ticklength, 16]}
+            />
+            <meshBasicMaterial
+              color={yColor}
+              depthWrite={true}
+              transparent={false}
+            />
+            {strLabelPos}
+          </mesh>
+        );
         ticksY.push(
-          <mesh position={[0, -i, 0]} rotation={[0, 0, Math.PI/2]} key={`ticky-${i}-neg`}> 
-            <cylinderGeometry args={[thickness/2, thickness/2, ticklength, 16]} />
-            <meshBasicMaterial color={yColor} />{strLabelNeg}</mesh>
-        )
+          <mesh
+            position={[0, -i, 0]}
+            rotation={[0, 0, Math.PI / 2]}
+            key={`ticky-${i}-neg`}
+          >
+            <cylinderGeometry
+              args={[thickness / 2, thickness / 2, ticklength, 16]}
+            />
+            <meshBasicMaterial
+              color={yColor}
+              depthWrite={true}
+              transparent={false}
+            />
+            {strLabelNeg}
+          </mesh>
+        );
       }
     }
   }
 
-  if (includeZ) { 
+  if (includeZ) {
     axes.push(
-      <mesh position={[0, 0, 0]} rotation={[Math.PI/2, 0, 0]} key={`axisz`}>
-        <cylinderGeometry args={[thickness, thickness, 2*depth, 16]} />
-        <meshBasicMaterial color={zColor} />
+      <mesh position={[0, 0, 0]} rotation={[Math.PI / 2, 0, 0]} key={`axisz`}>
+        <cylinderGeometry args={[thickness, thickness, 2 * depth, 16]} />
+        <meshBasicMaterial color={zColor} transparent={true} depthTest={true} />
       </mesh>
     );
     if (tickspacing > 0) {
-      for (var i=tickspacing; i<=depth; i+=tickspacing) {
-
+      for (var i = tickspacing; i <= depth; i += tickspacing) {
         strLabelPos = strLabelNeg = "";
         if (includeLabels) {
           strLabelPos = (
-            <Text key={`labelz-${i}-pos`}
-              position={[0, -(1 + ticklength/2), 0]} rotation={[0, Math.PI/2, 0]} 
-              anchorY="right" anchorZ="top" color={zColor} 
-              fontSize={thickness * tickspacing/3}>{i.toFixed(0)}</Text>
-          )
+            <Text
+              key={`labelz-${i}-pos`}
+              position={[0, -(1 + ticklength / 2), 0]}
+              rotation={[0, Math.PI / 2, 0]}
+              anchorY="right"
+              anchorZ="top"
+              color={zColor}
+              fontSize={(thickness * tickspacing) / 3}
+            >
+              {i.toFixed(0)}
+            </Text>
+          );
           strLabelNeg = (
-            <Text  key={`labelz-${i}-neg`}
-              position={[0, -(1 + ticklength/2), 0]} rotation={[0, Math.PI/2, 0]}
-              anchorY="right" anchorZ="top" color={zColor} 
-              fontSize={thickness * tickspacing/3}>-{i.toFixed(0)}</Text>
-          )
+            <Text
+              key={`labelz-${i}-neg`}
+              position={[0, -(1 + ticklength / 2), 0]}
+              rotation={[0, Math.PI / 2, 0]}
+              anchorY="right"
+              anchorZ="top"
+              color={zColor}
+              fontSize={(thickness * tickspacing) / 3}
+            >
+              -{i.toFixed(0)}
+            </Text>
+          );
         }
 
         ticksZ.push(
-          <mesh position={[0, 0, i]} rotation={[0, 0, 0]} key={`tickz-${i}`}> 
-            <cylinderGeometry args={[thickness/2, thickness/2, ticklength, 16]} />
-            <meshBasicMaterial color={zColor} />
+          <mesh position={[0, 0, i]} rotation={[0, 0, 0]} key={`tickz-${i}`}>
+            <cylinderGeometry
+              args={[thickness / 2, thickness / 2, ticklength, 16]}
+            />
+            <meshBasicMaterial
+              color={zColor}
+              depthWrite={true}
+              transparent={false}
+            />
             {strLabelPos}
           </mesh>
         );
         ticksZ.push(
-          <mesh position={[0, 0, -i]} rotation={[0, 0, 0]} key={`tickz-${i}-neg`}> 
-            <cylinderGeometry args={[thickness/2, thickness/2, ticklength, 16]} />
-            <meshBasicMaterial color={zColor} />
+          <mesh
+            position={[0, 0, -i]}
+            rotation={[0, 0, 0]}
+            key={`tickz-${i}-neg`}
+          >
+            <cylinderGeometry
+              args={[thickness / 2, thickness / 2, ticklength, 16]}
+            />
+            <meshBasicMaterial
+              color={zColor}
+              depthWrite={true}
+              transparent={false}
+            />
             {strLabelNeg}
           </mesh>
         );
-
       }
     }
   }
@@ -148,9 +272,9 @@ export default function CartesianAxes({
     <>
       <group position={[0, 0, 0]}>
         {axes}
-        {(includeX)?<group>{ticksX}</group>:<></>}
-        {(includeY)?<group>{ticksY}</group>:<></>}
-        {(includeZ)?<group>{ticksZ}</group>:<></>}
+        {includeX ? <group>{ticksX}</group> : <></>}
+        {includeY ? <group>{ticksY}</group> : <></>}
+        {includeZ ? <group>{ticksZ}</group> : <></>}
       </group>
     </>
   );
